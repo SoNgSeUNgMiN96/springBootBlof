e93068ca75e60f829af38ad5c38ed88b98899613
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2022-06-09 01:52:44 +0900

    CI CD Testing

--- src/main/java/com/blog/springboot/Application.java
+++ src/main/java/com/blog/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing  //JPA Auditing 삭제
+//@EnableJpaAuditing  //JPA Auditing 삭제 //testing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
