92b67519e71309f5f2f9c99fa2a9824de183712c
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2022-06-08 17:05:41 +0900

    testing 환경 연동완료
    작성일 : 2022-06-08

--- src/test/java/com/blog/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/blog/springboot/domain/posts/PostsRepositoryTest.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@TestPropertySource("classpath:application-test.properties")
 public class PostsRepositoryTest {
 
     @Autowired
