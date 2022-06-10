a720b9a53c066aef24e114ed9a43e9520f234f01
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2022-06-10 16:23:36 +0900

    deploy.sh 실행중인 pid 확인 명령 수정
    작성일  2022-06-10

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "현재 구동중인 애플리케이션 pid확인 "
 
-CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar |. wk '{print $1}')
+CURRENT_PID=$(pgrep -fl springBootBlog | grep jar | awk '{print $1}')
 
 
 echo "현재 구동중인 PID = $CURRENT_PID"
